968ffc608ee1f3bbd92def04518f4516b6149f20
nivnis <nivhaim.nisani@live.biu.ac.il>
2020-03-29 20:17:46 +0300

    view resizes

--- FlightSimulatorApp/MainWindow.xaml.cs
+++ FlightSimulatorApp/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Sockets;
+using System.Windows.Interop;
 using FlightSimulatorApp.Models;
 using FlightSimulatorApp.ViewModels;
 using FlightSimulatorApp.Views;
@@ -49,58 +50,8 @@ namespace FlightSimulatorApp
             cw = new ConnectWindow(flightSimulatorViewModel);
         }
 
-        //private void SetXY(double x, double y)
-        //{
-        //    if (x > 1)
-        //    {
-        //        x = 1;
-        //    } 
-        //    else if (x < -1)
-        //    {
-        //        x = -1;
-        //    }
-
-        //    if (y > 1)
-        //    {
-        //        y = 1;
-        //    }
-        //    else if (y < -1)
-        //    {
-        //        y = -1;
-        //    }
-        //    flightSimulatorViewModel.VM_Rudder = x;
-        //    flightSimulatorViewModel.VM_Elevator = y;
-        //    RudderValue.Text = x.ToString();
-        //    ElevatorValue.Text = y.ToString();
-        //}
-
-		private void Throttle_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-		{
-
-		}
-
-		private void Joystick_Loaded(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
-        //      private void AileronSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        //      {
-        //          AileronValue.Text = AileronSlider.Value.ToString("F" );
-        //          Console.WriteLine(AileronSlider.Value.ToString());
-        //          Console.WriteLine(sender.ToString());
-        //      }
-
-        //      private void ThrottleSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        //      {
-        //          ThrottleValue.Text = ThrottleSlider.Value.ToString("F");
-        //      }
-
-
         private void connect_Click(object sender, RoutedEventArgs e)
         {
-            //connectWindow.Show();
-            //this.flightSimulatorViewModel.VM_ConnectWindow();
             cw.Show();
         }
 
